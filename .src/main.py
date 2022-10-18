0eed887f9f8d6e16805da39aaa304d3573b68dd1
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-10-18 20:50:30 +0530

    Update main.py

--- .src/main.py
+++ .src/main.py
@@ -1,1 +1,8 @@
+import os,sys
 
+print("\n")
+pa = input("[!] Enter UPI ID: ")
+pn = input("[?] Enter Name: ")
+am = input("[?] Enter Amount: ")
+
+print("UPI is Ready: UPI://pay?pa={0}&pn={1}&am{2}".format(pa,pn,am))      
